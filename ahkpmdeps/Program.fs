83532bef3aa11d1a43f419d56dcbc63646d41223
Paul Staszko <paulstaszko@gmail.com>
2024-08-09 20:45:48 -0500

    ok

---
+++ ahkpmdeps/Program.fs
@@ -0,0 +1,16 @@
+﻿[<EntryPoint>]
+let main args =
+    let cd = System.IO.Directory.GetCurrentDirectory()
+    let getLockContent() =
+        try
+            System.IO.File.ReadAllText "ahkpm.lock"
+            |> Ok
+        with ex -> ex.Message |> Error
+    getLockContent()
+    |> Result.bind (fun content ->
+        let d =System.Text.Json.JsonDocument.Parse(content)
+
+        Ok ()
+    )
+    |> ignore
+    0
